d648f827f9c308f2d5a7489c3e4e0d560076b231
amarrianto <amarrianto6969@gmail.com>
2021-04-05 01:02:05 +0700

    ngantuk

---
+++ object.js
@@ -0,0 +1,17 @@
+// variable di dalam object disebut properti, function didalam object disebut method
+
+var mhs = {
+    nama : "Amar Rianto",
+    umur : 25,
+    ips : [2.25,3.21,3.56,4],
+    alamat : {
+        jalan : "jl.abc No.123",
+        kota : "depok",
+        provinsi : "Jawa barat"
+    }
+}
+
+// menampilkan
+// mhs.nama
+// mhs.ips[3]
+// mhs.alamat.kota
