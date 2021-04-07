464b5ef9c861a64e12db2b0941414b49ac8d30a7
amarrianto <amarrianto6969@gmail.com>
2021-04-07 15:32:25 +0700

    rabu

--- Javasciprt Lanjutan/object.js
+++ Javasciprt Lanjutan/object.js
@@ -1,19 +1,60 @@
 // cara membuat object pd javascript
 // 1.object literal 
-let mahasiswa1 = {
-    nama: 'Amar',
-    energy: 10,
-    makan : function(porsi){
-        this.energy += porsi;
-        console.log(`Hallo ${this.nama}, Selamat makan`)
+// let mahasiswa1 = {
+//     nama: 'Amar',
+//     energy: 10,
+//     makan : function(porsi){
+//         this.energy += porsi;
+//         console.log(`Hallo ${this.nama}, Selamat makan`)
+//     }
+// }
+
+// let mahasiswa2 = {
+//     nama: 'Bowo',
+//     energy: 20,
+//     makan : function(porsi){
+//         this.energy += porsi;
+//         console.log(`Hallo ${this.nama}, Selamat makan`)
+//     }
+// }
+
+// function declaration
+
+// function Mahasiswa(nama,energi){
+//     let mahasiswa = {}
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi
+
+//     mahasiswa.makan = function(porsi){
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan`)
+//     }
+    
+//     mahasiswa.main = function(jam){
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat main`)
+//     }
+//     return mahasiswa
+// }
+
+// let amar = Mahasiswa('Amar',18);
+// let sidik = Mahasiswa('Sidik', 20);
+
+//constructor
+function Mahasiswa(nama,energi){
+    this.nama = nama;
+    this.energi = energi
+
+    this.makan = function(porsi){
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan`)
+    }
+    
+    this.main = function(jam){
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat main`)
     }
 }
 
-let mahasiswa2 = {
-    nama: 'Bowo',
-    energy: 20,
-    makan : function(porsi){
-        this.energy += porsi;
-        console.log(`Hallo ${this.nama}, Selamat makan`)
-    }
-}
+let amar = new Mahasiswa('Amar',10);
+let sidik = Mahasiswa('Sidik', 20);
