2f002e1f668930457fe3f95cf198eb9838593934
amarrianto <amarrianto6969@gmail.com>
2021-04-12 00:43:59 +0700

    context-hoisting-scope

---
+++ Javasciprt Lanjutan/executionContext-hoisting-scope.js
@@ -0,0 +1,14 @@
+function satu() {
+  var nama = "amar";
+  console.log(nama);
+}
+
+function dua() {
+  console.log(nama);
+}
+
+console.log(nama);
+var nama = "Bagus";
+satu();
+dua("Doddy");
+console.log(nama);
