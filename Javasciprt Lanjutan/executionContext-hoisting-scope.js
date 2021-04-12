622839543a756a540583bcc3940d076372767fa8
amarrianto <amarrianto6969@gmail.com>
2021-04-12 23:23:34 +0700

    execution-hoisting-scoope

--- Javasciprt Lanjutan/executionContext-hoisting-scope.js
+++ Javasciprt Lanjutan/executionContext-hoisting-scope.js
@@ -1,14 +1,38 @@
-function satu() {
-  var nama = "amar";
-  console.log(nama);
-}
+// var nama = "Amar";
+// var umur = 25;
 
-function dua() {
-  console.log(nama);
-}
+// console.log(sayHello());
 
-console.log(nama);
-var nama = "Bagus";
-satu();
-dua("Doddy");
-console.log(nama);
+// function sayHello() {
+//   return `Hallo, nama saya ${nama}, ${umur} tahun.`;
+// }
+
+function a() {
+  console.log("ini a");
+
+  function b() {
+    console.log("ini b");
+
+    function c() {
+      console.log("ini c");
+    }
+    c();
+  }
+  b();
+}
+a();
+
+// function satu() {
+//   var nama = "amar";
+//   console.log(nama);
+// }
+
+// function dua() {
+//   console.log(nama);
+// }
+
+// console.log(nama);
+// var nama = "Bagus";
+// satu();
+// dua("Doddy");
+// console.log(nama);
