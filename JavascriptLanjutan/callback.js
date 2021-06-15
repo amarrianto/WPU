7488f06867097a0eb767de30cfd4349756aa645c
Amar Rianto <amarrianto6969@gmail.com>
2021-06-15 23:27:57 +0700

    asynchronous with ajax

--- JavascriptLanjutan/callback.js
+++ JavascriptLanjutan/callback.js
@@ -46,29 +46,43 @@
 
 //Asynchronous
 
-function getDataMahasiswa(url, success, error) {
-  let xhr = new XMLHttpRequest();
+// function getDataMahasiswa(url, success, error) {
+//   let xhr = new XMLHttpRequest();
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        success(xhr.response);
-      } else if (xhr.status === 404) {
-        error();
-      }
-    }
-  };
-  xhr.open("get", url);
-  xhr.send();
-}
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         success(xhr.response);
+//       } else if (xhr.status === 404) {
+//         error();
+//       }
+//     }
+//   };
+//   xhr.open("get", url);
+//   xhr.send();
+// }
 
-console.log("mulai");
-getDataMahasiswa(
-  "data/mahasiswa.json",
-  (results) => {
-    const mhs = JSON.parse(results);
-    mhs.forEach((m) => console.log(m.nama));
+// console.log("mulai");
+// getDataMahasiswa(
+//   "data/mahasiswa.json",
+//   (results) => {
+//     const mhs = JSON.parse(results);
+//     mhs.forEach((m) => console.log(m.nama));
+//   },
+//   () => {}
+// );
+// console.log("selesai");
+
+// JQuery
+// console.log('mulai');
+$.ajax({
+  url: "data/mahasiswa.json",
+  success: (mhs) => {
+    mhs.forEach(m => console.log(m.nama));
   },
-  () => {}
-);
-console.log("selesai");
+  error: (e) => {
+    console.log(e.responseText);
+  },
+});
+
+// console.log('selesai');
